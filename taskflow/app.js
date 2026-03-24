01236a5237a209c3c2a677fb856a6d265fd8357d
Samarth <achar.samarth@gmail.com>
2026-03-24 12:00:00 +0000

    Changes Made: Finished Variables and Functions

--- taskflow/app.js
+++ taskflow/app.js
@@ -1,1 +1,21 @@
 console.log("TaskFlow loaded!");
+
+const tasks = [
+{ id: 1, title: "Install my project folder", done: true },
+{ id: 2, title: "Learn JS variables", done: true },
+{ id: 3, title: "Build TaskFlow UI", done: false },
+{ id: 4, title: "Finish Checkpoint", done: true },
+
+];
+console.log(tasks);
+
+function printTasks(taskArray) {
+    for (const task of taskArray) {
+        if (task.done === true) {
+            console.log("[DONE]", task.title);
+        } else {
+            console.log("[TODO]", task.title);
+        }
+    }
+}
+printTasks(tasks);
